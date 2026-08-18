6f76c79ceeea26dc02634f55cf03627ac2060e50
westmontdp <westmontdp@gmail.com>
2026-08-18 11:05:33 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const observer = new IntersectionObserver((entreis) => {
+  entries.forEach((entry) => {
+    consle.log(entry)
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
 
+const hiddenElements = document.querySelectorAll('.hidden');
